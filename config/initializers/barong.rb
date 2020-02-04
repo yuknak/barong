e7a097e66d4179a439d781265e0c749110e4518e
yuknak <yuknak4@gmail.com>
2020-02-04 14:04:14 +0900

    Using fixed priv key for test

--- config/initializers/barong.rb
+++ config/initializers/barong.rb
@@ -20,9 +20,10 @@ begin
     Rails.logger.info('Loading private key from credentials.yml.enc')
 
   elsif !Rails.env.production?
-    pkey = Barong::KeyStore.generate
-    Rails.logger.warn('Warning !! Generating private key')
-
+    #pkey = Barong::KeyStore.generate
+    #Rails.logger.warn('Warning !! Generating private key')
+    Rails.logger.warn('Warning !! Using test private key')
+    pkey = "-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAxcFCteyZ0w+KORQo88yaM20YIDFLLbPMOeZLRzmIBsgW/18l\nyZFKbuFfcL1O2c4y93Ag5xCwIrrsrZIOmEudsw1Cbu+hR4JnROS8nWgYxgExC/KJ\noeZi0N5+WNAjvOBxPdovk0W+z20FCecD964qAtVmXc7W1NuVX4CbWv/7IljHk3zX\n464ZThTrCBKuEfjLsK3I7aPbtSf4xFT90Mmn/poJaekjfXUGHH997OEvDdE0SvxT\ntBg1jq6OnrxhMUBvPrN6ccCV67xOOBT+rKyx5Zzgxa68m2CoIhCOMjQAlRB4Svu2\nOW8eb+FlgFlS4VOeRm6NICB0hHyPH1hLn8OvewIDAQABAoIBAHzxmTOWVK4skFl1\n6lX3PKonUGnumyA6DFu8rG1I5S/bteQNerN7D7s0u/dgNHSaukrp1nAHdDHNRoJ3\n2K7Sf8XEJ+gtkQm8U4EMwDr3rUdVM2boC5t2E5MCMHPutdW9PO8mbo6vL1qg9+EE\ny9XufW6i4/V7HPMJ9buMZxu8xjAxKMDr2ssWD/fR7i53AFEw6LjfcWKzrjTuoJMa\nvcquyymbkNGw+1bTs/rjZNMGgb874FIEiJsGa80lzri7CaE89W22qrt0bs/b0n9m\npOeIFp3QQ7FrWl6040oYt72XyIKbcScuFNd7FEi1pmZYPQh5xWTTPOvWEE3hy9Jk\npFuX6MkCgYEA7o86t7KIPE1eFuMgM3EuBTeRpBBnOAqwXWuvjSSUdLweyqF8+kU1\nGO7KCVDUZBnAd5QjXxuZjO4E0CNDoTh1m/bElwGopyaJG6b+ombSOD6K8KV9lM8s\nLVMPl86mHFqkHeHcSx6WWC4cENzGGqMkRAEJaKA1L+jDlF7EVKssMKUCgYEA1DZa\nASMzdVW5fkwmQ+Yu84uJ/lvHzWzSfjjXY7FkLqa22sQ7YwijRlVJfE93Bb0MpBJD\nZW34nck9zdlH2zEa9YpYZY+RVl6BFsmKBXTqNXW9ixfCPf+vca7sLYQ+hpdlfGE5\n53mv8Lax+odUT2ReloQhqW4G4NjqqG/X+VpRRZ8CgYAcoCMzl8CxO7dml6ptgc87\n4Qcg4LcCCoZPL3TJQvJtb4ViWy5b+aH9c+naZFCYEl79+lCkQPOT7Zu2PgUHe3bh\nWCMO26wZIo6hOtCjPCNNjHOvnKwNBy8N4UGlva5BCL9YtplwiiMQQbgsbdF3sMvR\nPS6b112Keiu9ygFVB5Ut4QKBgDXtMeYEGfmBNUgA9eBfSCMfnIuEqztqofrtWt68\nn2azetgQ8b5y3XrsBWaPkwFkTygKdBH8ZidCknAS/Q2YZu9qnpgAacB293rg8C/+\ny92V8/q6qOO0a9MJzn6qknEFXAbFdj96Ttlus7+kUCp0qQy6uwshKKJOvSLceRem\neeylAoGBANcbaYE/pErX3ZxL5RLDA6GhdcSO3DJHEjNOxgxApE/Pd0POMtfD4fiG\nHNTSRU8FWP9xkUJ/v37pe8jnGg1tb5qYf9IJuiC5TxWYnwuhuyLdprm82FAcaVKr\nS5a22NlfRn3A4D4uZQ9ilRMV61YvXGofspZjugC54oL3zEWfcdU5\n-----END RSA PRIVATE KEY-----\n"
   else
     raise Barong::KeyStore::Fatal
   end
